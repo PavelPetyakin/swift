85e47b29f5e3c7a2622f16ae87f5707720b32bb0
Pavel Petyakin <pavel.petyakin@jetrockets.pro>
2021-06-18 00:29:20 +0300

    Initial Commit

---
+++ IOSLandmarks/IOSLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSLandmarksApp.swift
+//  IOSLandmarks
+//
+//  Created by Pavel Petyakin on 18.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct IOSLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
