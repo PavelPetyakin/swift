307ec4d47473521f2e8aed301bd8c86435e75175
Pavel Petyakin <petyakin86@mail.ru>
2021-10-30 21:04:43 +0300

    first commit

---
+++ IOSLandmarks/Model/ModelData.swift
@@ -0,0 +1,33 @@
+//
+//  ModelData.swift
+//  IOSLandmarks
+//
+//  Created by Pavel Petyakin on 22.06.2021.
+//
+
+import Foundation
+
+var landmarks: Array<Landmark> = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couidn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+    
+}
---
+++ IOSLandmarks/Views/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  IOSLandmarks
+//
+//  Created by Pavel Petyakin on 18.06.2021.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    var body: some View {
+        image
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(Circle()             .stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
